3c4a8793b69e0367582b414a69ef51c9c7c5df53
samuelnainggolan <samuelleonainggolan@gmail.com>
2026-09-16 17:31:48 +0700

    Update latihan4.py

--- latihan4.py
+++ latihan4.py
@@ -9,7 +9,7 @@ from collections import Counter
 # =========================================================
 
 st.set_page_config(
-    page_title="AI Tutor Bahasa Indonesia",
+    page_title="AI Tutor Bahasa Indonesia, samuel",
     page_icon="📚",
     layout="wide"
 )
